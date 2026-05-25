c1e2c2c3d20e61d1e1f607bf3465a61302689012
vanessa446 <templevanessa446@gmail.com>
2026-05-25 14:25:28 +0100

    chapter 3

--- chapter 3/Decrypt .java
+++ chapter 3/Decrypt .java
@@ -1,29 +1,31 @@
+
+Decryption Program:
 import java.util.Scanner;
 
-public class Encrypt {
+public class Decrypt {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a 4-digit integer: ");
+        System.out.print("Enter encrypted 4-digit integer: ");
         int number = scanner.nextInt();
 
-        
+        // Extract digits
         int d1 = number / 1000;
         int d2 = (number / 100) % 10;
         int d3 = (number / 10) % 10;
         int d4 = number % 10;
 
         
-        d1 = (d1 + 7) % 10;
-        d2 = (d2 + 7) % 10;
-        d3 = (d3 + 7) % 10;
-        d4 = (d4 + 7) % 10;
-
-        
         int temp = d1; d1 = d3; d3 = temp;
         temp = d2; d2 = d4; d4 = temp;
 
-        int encrypted = d1 * 1000 + d2 * 100 + d3 * 10 + d4;
-        System.out.printf("Encrypted number: %d%n", encrypted);
+        
+        d1 = (d1 + 3) % 10;
+        d2 = (d2 + 3) % 10;
+        d3 = (d3 + 3) % 10;
+        d4 = (d4 + 3) % 10;
+
+        int decrypted = d1 * 1000 + d2 * 100 + d3 * 10 + d4;
+        System.out.printf("Decrypted number: %d%n", decrypted);
         scanner.close();
     }
 }
